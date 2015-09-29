23e82e811f252778adc57c0b33b6e00c5c1dd015
Lars Janssen <lars@fazy.net>
2015-09-29 17:57:02 +0100

    Reformat with gofmt

--- env-template.go
+++ env-template.go
@@ -1,45 +1,51 @@
 package main
 
 import (
-    "bytes"
-    "fmt"
-    "io/ioutil"
-    "os"
-    "text/template"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"text/template"
 )
 
 var templateFile string
 
 func init() {
-    if len(os.Args) != 2 {
-        exitError(fmt.Errorf("Usage: %s <template>\n", os.Args[0]))
-    }
+	if len(os.Args) != 2 {
+		exitError(fmt.Errorf("Usage: %s <template>\n", os.Args[0]))
+	}
 
-    templateFile = os.Args[1]
+	templateFile = os.Args[1]
 }
 
 func main() {
-    getEnv := func(key string) (string) {
-        value := os.Getenv(key)
-        return value
-    }
+	getEnv := func(key string) string {
+		value := os.Getenv(key)
+		return value
+	}
 
-    funcs := template.FuncMap{ "env": getEnv }
+	funcs := template.FuncMap{"env": getEnv}
 
-    templateContent, err := ioutil.ReadFile(templateFile)
-    if err != nil { exitError(err) }
+	templateContent, err := ioutil.ReadFile(templateFile)
+	if err != nil {
+		exitError(err)
+	}
 
-    parsedTemplate, err := template.New("test").Funcs(funcs).Parse(string(templateContent))
-    if err != nil { exitError(err) }
+	parsedTemplate, err := template.New("test").Funcs(funcs).Parse(string(templateContent))
+	if err != nil {
+		exitError(err)
+	}
 
-    var output bytes.Buffer
-    err = parsedTemplate.Execute(&output, nil)
-    if err != nil { exitError(err) }
+	var output bytes.Buffer
+	err = parsedTemplate.Execute(&output, nil)
+	if err != nil {
+		exitError(err)
+	}
 
-    fmt.Print(output.String())
+	fmt.Print(output.String())
 }
 
 func exitError(err error) {
-    fmt.Println(err)
-    os.Exit(1)
+	fmt.Println(err)
+	os.Exit(1)
 }
